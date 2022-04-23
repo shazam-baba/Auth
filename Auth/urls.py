67f68d8cb6d075ab87d17071d62132357d938cbe
Siddharh Sharma <62809024+shazam-baba@users.noreply.github.com>
2022-04-23 14:12:11 +0530

    final

--- Auth/urls.py
+++ Auth/urls.py
@@ -7,6 +7,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html")),
-    path('accounts/', include('allauth.urls')),
-    path('logout', LogoutView.as_view(),name='logout'),
+    path('accounts/', include('allauth.urls')), #Django Allauth Library
+    path('logout', LogoutView.as_view(),name='logout'), #Logout function
 ]
